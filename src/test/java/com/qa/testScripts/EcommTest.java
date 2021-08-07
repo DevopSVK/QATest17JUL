df5c53f9ad26f6bafedbc2f8990dce16d5f40847
DevopSVK <86151829+DevopSVK@users.noreply.github.com>
2021-08-07 20:19:44 +0530

    Update EcommTest.java

--- src/test/java/com/qa/testScripts/EcommTest.java
+++ src/test/java/com/qa/testScripts/EcommTest.java
@@ -3,6 +3,7 @@ package com.qa.testScripts;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
@@ -24,28 +25,24 @@ import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 public class EcommTest {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @Before
-  public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-  }
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
   @Test
-  public void ecomm() {
-    driver.get("http://172.31.37.152:8181/");
+  public void setUp()  throws InterruptedException
+  {
+    WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+     driver.get("http://172.31.37.152:8181/");
+    driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+    Thread.sleep(2000); 
     driver.manage().window().setSize(new Dimension(1550, 838));
     Thread.sleep(2000);
     driver.findElement(By.cssSelector(".hero-content > .btn")).click();
+    Thread.sleep(2000);
     driver.findElement(By.linkText("Shop")).click();
+    Thread.sleep(2000);
     driver.findElement(By.linkText("Dresses")).click();
+    Thread.sleep(2000);
     driver.findElement(By.cssSelector(".col-12:nth-child(1) .hover-img")).click();
+    Thread.sleep(2000);
     driver.findElement(By.cssSelector(".col-12:nth-child(1) .btn")).click();
   }
 }
